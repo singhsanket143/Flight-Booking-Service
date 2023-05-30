99854a23e09149e325ddb176c3d523426a12d0f0
Sanket Singh <singhsanket143@gmail.com>
2023-05-30 20:30:39 +0530

    Added booking logic with transactions

--- src/repositories/booking-repository.js
+++ src/repositories/booking-repository.js
@@ -7,6 +7,11 @@ class BookingRepository extends CrudRepository {
     constructor() {
         super(Booking);
     }
+
+    async createBooking(data, transaction) {
+        const response = await Booking.create(data, {transaction: transaction});
+        return response;
+    } 
 }
 
 module.exports = BookingRepository;
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -6,18 +6,30 @@ const { ServerConfig } = require('../config')
 const db = require('../models');
 const AppError = require('../utils/errors/app-error');
 
+const bookingRepository = new BookingRepository();
+
 async function createBooking(data) {
-    return new Promise((resolve, reject) => {
-        const result = db.sequelize.transaction(async function bookingImpl(t) {
-            const flight = await axios.get(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`);
-            console.log(typeof flight);
-            const flightData = flight.data.data;
-            if(data.noofSeats > flightData.totalSeats) {
-                reject(new AppError('Not enough seats available', StatusCodes.BAD_REQUEST));
-            }
-            resolve(true);
+    const transaction = await db.sequelize.transaction();
+    try {
+        const flight = await axios.get(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`);
+        const flightData = flight.data.data;
+        if(data.noofSeats > flightData.totalSeats) {
+            throw new AppError('Not enough seats available', StatusCodes.BAD_REQUEST);
+        }
+        const totalBillingAmount = data.noofSeats * flightData.price;
+        const bookingPayload = {...data, totalCost: totalBillingAmount};
+        const booking = await bookingRepository.create(bookingPayload, transaction);
+
+        await axios.patch(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}/seats`, {
+            seats: data.noofSeats
         });
-    })
+
+        await transaction.commit();
+        return booking;
+    } catch(error) {
+        await transaction.rollback();
+        throw error;
+    }
     
 }
 
